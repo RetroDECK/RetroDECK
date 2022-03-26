145ff8766abb37cc3435848011ac62d5a3d957d9
XargonWan <xargonwan@gmail.com>
2022-03-26 17:45:56 +0900

    Added shared modules, preparing for yuzu integration.

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -13,21 +13,29 @@ sudo pacman -S flatpak-builder
 flatpak install org.kde.Sdk//5.15-21.08 org.kde.Platform//5.15-21.08
 
 cd $INSTALL_DIR
-# To update change branch and update the manifest with the retroarch data here:
+
+# External flatpaks import
+# To update change branch and update the manifest.
+# Some json must be converted with this: https://codebeautify.org/json-to-yaml
+
+# RetroArch
 # https://github.com/flathub/org.libretro.RetroArch/blob/master/org.libretro.RetroArch.json
-# must be converted with this: https://codebeautify.org/json-to-yaml
 git clone --recursive --branch update-v1.10.2 https://github.com/flathub/org.libretro.RetroArch.git
 # removing not needed and potentially dangerous files
-rm -f org.libretro.RetroArch/retroarch.cfg
-rm -f org.libretro.RetroArch/README.md
-rm -f org.libretro.RetroArch/org.libretro.RetroArch.json
-rm -f org.libretro.RetroArch/COPYING
-rm -f org.libretro.RetroArch/.*
-mv -n org.libretro.RetroArch/* $INSTALL_DIR
+#rm -rf org.libretro.RetroArch/shared-modules
+#rm -f org.libretro.RetroArch/retroarch.cfg
+#rm -f org.libretro.RetroArch/README.md
+#rm -f org.libretro.RetroArch/org.libretro.RetroArch.json
+#rm -f org.libretro.RetroArch/COPYING
+#rm -rf org.libretro.RetroArch/.*
+ln -s org.libretro.RetroArch/* $INSTALL_DIR/
 
-
-
-
+# Yuzu
+# https://github.com/flathub/org.yuzu_emu.yuzu/blob/master/org.yuzu_emu.yuzu.json
+git clone --recursive https://github.com/flathub/org.yuzu_emu.yuzu
+#rm -rf org.yuzu_emu.yuzu/shared-modules
+#rm -rf org.yuzu_emu.yuzu/.*
+ln -s org.yuzu_emu.yuzu/* $INSTALL_DIR/
 
 
 cd $INSTALL_DIR
@@ -43,10 +51,12 @@ flatpak-builder retrodeck-flatpak com.xargon.retrodeck.yml --force-clean
 # flatpak run --command=/bin/bash com.xargon.retrodeck
 
 # Cleaning up
-# do you want to remove the downloaded data?
-# if so:
-rm -rf org.libretro.RetroArch
+#rm -rf org.libretro.RetroArch
+#rm -rf org.yuzu_emu.yuzu
 
-echo "Building terminated, you can install retrodeck by typing `flatpak run com.xargon.retrodeck`."
+# removing orphaned symlinks
+find -L . -name . -o -type d -prune -o -type l -exec rm {} +
+
+echo "Building terminated, you can install retrodeck by typing: flatpak run com.xargon.retrodeck"
 
 cd $PREVIOUS_DIR
