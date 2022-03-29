1946df5fab1f0821b6cd6a28939413e5a484da18
XargonWan <xargonwan@gmail.com>
2022-03-29 11:51:22 +0900

    - Added tools section but needs fixing

--- retrodeck.sh
+++ retrodeck.sh
@@ -7,13 +7,20 @@ then
     mkdir -p ~/retrodeck/saves
     mkdir -p ~/retrodeck/states
     mkdir -p ~/retrodeck/screenshots
+    mkdir -p ~/retrodeck/tools
     mkdir -p /var/config/retroarch/system
+    
     ln -s ~/.var/app/com.xargon.retrodeck/config/retroarch/system/ ~/retrodeck/bios
     rm -rf ~/retrodeck/.emulationstation/es_settings.xml
     rm -rf ~/retrodeck/.emulationstation/es_input.xml
     cp /app/retrodeck/es_settings.xml ~/retrodeck/.emulationstation/es_settings.xml
     cp /app/retrodeck/es_settings.xml ~/retrodeck/.emulationstation/es_input.xml
     cp /app/retrodeck/retrodeck-retroarch.cfg /var/config/retroarch/retroarch.cfg
+    cp -r /app/retrodeck/tools/* ~/retrodeck/tools/
+
+    mkdir -p ~/retrodeck/.emulationstation/gamelists/tools/
+    cp /app/retrodeck/tools-gamelist.xml ~/retrodeck/.emulationstation/gamelists/tools/gamelist.xml
+    
     touch ~/retrodeck/.lock
 fi
 
