1543fdd49380c53e6166a18c1f16aefb370f9c8e
XargonWan <xargonwan@gmail.com>
2022-03-30 23:14:43 +0900

    - Fixed core location issue - Improved startup script - Added kdialog

--- tools/start-retroarch.sh
+++ tools/start-retroarch.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-retroarch
+yousure=kdialog --title "RetroDECK" --warningyesno "Doing some changes in the RetroArch configuration may create serious issues, please continue only if you know what you're doing. Are you sure do you want to continue?"
+if $yousure then
+    retroarch
