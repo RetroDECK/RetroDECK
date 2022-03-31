82b1e4612900c027651613d1b8e0efc8fbff26bd
XargonWan <xargonwan@gmail.com>
2022-03-31 20:50:46 +0900

    Fixed startup script (finally)

--- retrodeck.sh
+++ retrodeck.sh
@@ -23,49 +23,52 @@ then
         fi
     fi
 
-    kdialog --title "RetroDECK" --msgbox "RetroDECK will initialize the system, please wait a few minutes, a popup will tell you when the process is finished."
+    # initializing ES-DE
 
-    mkdir -p $roms_folder
-    rm -rf /var/config/.emulationstation/ROMs
-    rm -rf /var/config/.emulationstation/roms
-    mkdir -p /var/config/.emulationstation
-    ln -s $roms_folder /var/config/.emulationstation/roms
+    mkdir -p /var/config/retrodeck/tools
 
-    mkdir -p /var/config/.emulationstation
-    rm -rf /var/config/.emulationstation/es_settings.xml
-    rm -rf /var/config/.emulationstation/es_input.xml
-    cp /app/retrodeck/es_settings.xml /var/config/.emulationstation/es_settings.xml
-    cp /app/retrodeck/es_settings.xml /var/config/.emulationstation/es_input.xml
+    # Cleaning
+    rm -rf /var/config/emulationstation/
+    rm ~/retrodeck/bios
+    rm /var/config/retrodeck/tools/*
+
+    ls -ln /var/config/ #DEBUG
+    ls -ln /var/config/emulationstation/.emulationstation #DEBUG
+    read -n 1 -r -s -p $'Press enter to continue...\n' #DEBUG
+
+    kdialog --title "RetroDECK" --msgbox "EmulationStation will now initialize the system, please don't edit the rom location.\nJust select CREATE DIRECTORIES, YES, QUIT buttons.\nRetroDECK will manage the rest."
+
+    mkdir -p /var/config/emulationstation/
+
+    emulationstation --home /var/config/emulationstation
+
+    mv /var/config/emulationstation/ROMs /var/config/emulationstation/ROMs.bak
+    ln -s $roms_folder /var/config/emulationstation/ROMs
+    mv /var/config/emulationstation/ROMs.bak $roms_folder
+
+    # XMLSTARLET HERE
+    cp /app/retrodeck/es_settings.xml /var/config/emulationstation/.emulationstation/es_settings.xml
 
     mkdir -p ~/retrodeck/saves
     mkdir -p ~/retrodeck/states
     mkdir -p ~/retrodeck/screenshots
 
-    mkdir -p /var/config/retrodeck/tools
     cp -r /app/retrodeck/tools/* /var/config/retrodeck/tools
 
     mkdir -p /var/config/retroarch/system
     ln -s ~/.var/app/com.xargon.retrodeck/config/retroarch/system ~/retrodeck/bios
-        
+
     cp /app/retrodeck/retrodeck-retroarch.cfg /var/config/retroarch/retroarch.cfg
-    
-    mkdir -p /var/config/.emulationstation/gamelists/tools/
-    cp /app/retrodeck/tools-gamelist.xml /var/config/.emulationstation/gamelists/tools/gamelist.xml
+
+    mkdir -p /var/config/emulationstation/.emulationstation/gamelists/tools/
+    cp /app/retrodeck/tools-gamelist.xml /var/config/emulationstation/.emulationstation/custom_systems/tools/gamelist.xml
 
     mkdir -p /var/config/retroarch/cores/
     cp /app/share/libretro/cores/* /var/config/retroarch/cores/
-    
+
     touch ~/retrodeck/.lock
 
     kdialog --title "RetroDECK" --msgbox "Initialization completed, please put your roms in: $roms_folder.\nIf you wish to change the roms location you may use the tool located the tools section of RetroDECK."
+else
+    emulationstation --home /var/config/emulationstation
 fi
-
-#numFields=$(xmlstarlet sel -t -m '//system' -o "." /app/share/emulationstation/resources/systems/unix/es_systems.xml | wc -c)
-#for i in $(seq 1 $numFields); do
-#    system=$(xmlstarlet sel -t -m "//system[$i]" -v "name" /app/share/emulationstation/resources/systems/unix/es_systems.xml)
-#    if test -d ~/retrodeck/roms/${system}; then
-#        mkdir -p ~/retrodeck/roms/${system}
-#    fi
-#done
-
-emulationstation --home /var/config/
