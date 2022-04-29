27b8cce6111857167a81cb78544958aaf4d1781f
XargonWan <xargonwan@gmail.com>
2022-04-29 22:50:21 +0900

    CITRA: configured, needs testing

---
+++ tools/start-citra.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kdialog --title "RetroDECK" --warningyesno "Doing some changes in the emulator's configuration may create serious issues, please continue only if you know what you're doing.\n\nDo you want to continue?"
+if [ $? == 0 ]
+then
+    citra-qt
+fi
