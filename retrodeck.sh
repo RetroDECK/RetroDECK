6acfddb37fa2b076cf2119405d93aa37880bc167
xargon <xargonwan@gmail.com>
2022-07-18 13:00:33 +0200

    Fixed Xemu bios files and integrated the download of the free hd image

--- retrodeck.sh
+++ retrodeck.sh
@@ -151,6 +151,10 @@ standalones_init() {
     mkdir -pv $rdhome/saves/xemu
     cp -fv $emuconfigs/xemu.toml /var/data/xemu/xemu.toml
     sed -i 's#/home/deck/retrodeck#'$rdhome'#g' /var/data/xemu/xemu.toml
+    # Preparing HD dummy Image
+    wget "https://github.com/mborgerson/xemu-hdd-image/releases/latest/download/xbox_hdd.qcow2.zip" -P $rdhome/bios/
+    unzip $rdhome/bios/xbox_hdd.qcow2.zip $rdhome/bios/
+    rm -rfv $rdhome/bios/xbox_hdd.qcow2.zip
 
     # PICO-8
     # Moved PICO-8 stuff in the finit as only it knows here roms folders is
