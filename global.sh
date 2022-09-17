64ceec298d7ea833cf3bb5d7499a2acc4df7ad10
XargonWan <xargonwan@gmail.com>
2022-09-17 17:03:25 +0200

    GLOBAL.SH: added debug code to conf_write

--- global.sh
+++ global.sh
@@ -38,7 +38,7 @@ conf_write() {
   # writes the variables in the retrodeck config file
 
   echo "DEBUG: printing the config file content before writing it:"
-  cat "$rd_conf"
+  cat $rd_conf
   echo ""
 
   echo "Writing the config file: $rd_conf"
@@ -79,50 +79,8 @@ conf_write() {
     sed -i "s%themes_folder=.*%themes_folder=$themes_folder%" "$rd_conf"
   fi
 
-  if [ ! -z "$sdcard" ]
-  then
-    sed -i "s%sdcard=.*%sdcard=$sdcard%" "$rd_conf"
-  fi
   echo "DEBUG: New contents:"
-  cat "$rd_conf"
+  cat $rd_conf
   echo ""
 
 }
-
-# If there is no config file I initalize the file with the the default values
-if [ ! -f "$rd_conf" ]
-then
-
-  echo "RetroDECK config file not found in $rd_conf"
-  echo "Initializing"
-
-  # Initializing the variables
-  version="$hard_version"                                    # if we are here means that the we are in a new installation, so the version is valorized with the hardcoded one
-  rdhome="$HOME/retrodeck"                                   # the retrodeck home, aka ~/retrodeck
-  roms_folder="$rdhome/roms"                                 # the default roms folder path
-  saves_folder="$rdhome/saves"                               # the default saves folder path
-  states_folder="$rdhome/states"                             # the default states folder path
-  media_folder="$rdhome/downloaded_media"                    # the media folder, where all the scraped data is downloaded into
-  themes_folder="$rdhome/themes"                             # the themes folder
-  sdcard="$default_sd"                                       # Steam Deck SD default path
-
-  # Writing the variables for the first time
-  echo '#!/bin/bash'                          >> $rd_conf
-  echo "version=$version"                     >> $rd_conf
-  echo "rdhome=$rdhome"                       >> $rd_conf
-  echo "roms_folder=$roms_folder"             >> $rd_conf
-  echo "saves_folder=$saves_folder"           >> $rd_conf
-  echo "states_folder=$states_folder"         >> $rd_conf
-  echo "media_folder=$media_folder"           >> $rd_conf
-  echo "themes_folder=$themes_folder"         >> $rd_conf
-  echo "sdcard=$sdcard"                       >> $rd_conf
-
-  echo "Setting config file permissions"
-  chmod +rwx $rd_conf
-
-# If the config file is existing i just read the variables (source it)
-else
-  echo "Found RetroDECK config file in $rd_conf"
-  echo "Loading it"
-  source "$rd_conf"
-fi
