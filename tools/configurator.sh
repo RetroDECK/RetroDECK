377da5f30f37b0be28918d13da78d15ba359797c
icenine451 <benjamin.r.shelton@protonmail.com>
2022-10-04 12:21:46 -0400

    Create configurator.sh
    Start configurator skeleton

---
+++ tools/configurator.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+source /app/bin/global.sh # Store actual work code in globals (for use elsewhere if needed) and keep Configurator strictly dialogs
+
+# Configurator Option Tree
+
+# Welcome
+#     - Move Directories
+#         - Migrate ROM directory
+#         - Migrate downloaded_media
+#         - Migrate BIOS directory
+#     - Change Options
+#         - Change Rewind Setting
+#     - Upgrade Files
+#         - Grab all missing cores
+#         - Update all cores to nightly
+#     - RetroAchivement login
+#         - Login prompt
+#     - Reset RetroDECK
+#         - Reset RetroArch
+#         - Reset Standalone Emulators
+#         - Reset Tools
+#         - Reset All
+
+configurator_welcome_dialog() {
+
+}
+
+configurator_migration_dialog() {
+
+}
+
+configurator_options_dialog() {
+
+}
+
+configurator_update_dialog() {
+
+}
+
+configurator_retroachivement_dialog() {
+
+}
+
+configurator_reset_dialog() {
+
+}
