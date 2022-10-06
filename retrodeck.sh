4da0e6f1faef43767c50c9a8454afd8507343f44
icenine451 <benjamin.r.shelton@protonmail.com>
2022-10-06 18:53:38 -0400

    This whole line has got to go

--- retrodeck.sh
+++ retrodeck.sh
@@ -464,7 +464,6 @@ finit() {
     --ok-label "Cancel" \
     --extra-button "Internal" \
     --extra-button "SD Card" \
-    # --extra-button "Advanced"
     --text="Welcome to the first configuration of RetroDECK.\nThe setup will be quick but please READ CAREFULLY each message in order to avoid misconfigurations.\n\nWhere do you want your roms folder to be located?" )
     echo "Choice is $choice"
 
