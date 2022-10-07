30c690f885aba913f75497204ae0bb2046247cd4
XargonWan <XargonWan@gmail.com>
2022-10-07 08:48:35 +0200

    Removed check on folder creation
    The check is not needed as if mkdir finds the oqth just does nothing.

--- global.sh
+++ global.sh
@@ -67,10 +67,8 @@ conf_write() {
 # If there is no config file I initalize the file with the the default values
 if [ ! -f "$rd_conf" ]
 then
-  if [ ! -d /var/config/retrodeck ] # Check if the RetroDECK config directory exists
-  then
-    mkdir -p /var/config/retrodeck
-  fi
+  
+  mkdir -p /var/config/retrodeck
   echo "RetroDECK config file not found in $rd_conf"
   echo "Initializing"
   touch $rd_conf
