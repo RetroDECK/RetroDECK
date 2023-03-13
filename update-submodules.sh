062d92debe5fdda25f671db372c5df2148094e15
XargonWan <XargonWan@gmail.com>
2023-03-13 17:11:10 +0000

    Updated submodules

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-git submodule foreach --recursive git checkout master
-git submodule foreach --recursive git pull origin master
-
-git submodule foreach --recursive git checkout main
-git submodule foreach --recursive git pull origin main
+git submodule update --remote --merge --recursive
