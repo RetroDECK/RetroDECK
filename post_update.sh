47a754662aaa8cfe6936d5fa9bc733febf2248cb
icenine451 <59938822+icenine451@users.noreply.github.com>
2023-03-15 20:01:53 -0400

    dir_prep doesn't translate ~ correctly apparently

--- post_update.sh
+++ post_update.sh
@@ -195,7 +195,7 @@ post_update() {
     dir_prep "$rdhome/states/duckstation" "/var/data/duckstation/savestates"
 
     mv "$bios_folder/pico8" "$bios_folder/pico8_olddata" # Move legacy (and incorrect / non-functional ) PICO-8 location for future cleanup / less confusion
-    dir_prep "$bios_folder/pico-8" "~/.lexaloffle/pico-8" # Store binary and config files together. The .lexaloffle directory is a hard-coded location for the PICO-8 config file, cannot be changed
+    dir_prep "$bios_folder/pico-8" "$HOME/.lexaloffle/pico-8" # Store binary and config files together. The .lexaloffle directory is a hard-coded location for the PICO-8 config file, cannot be changed
     dir_prep "$roms_folder/pico8" "$bios_folder/pico-8/carts" # Symlink default game location to RD roms for cleanliness (this location is overridden anyway by the --root_path launch argument anyway)
     dir_prep "$bios_folder/pico-8/cdata" "$saves_folder/pico-8" # PICO-8 saves folder
   fi
