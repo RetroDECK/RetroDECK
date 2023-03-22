1025f1f98ca07e89ed3ce3135824ad604e1737b1
icenine451 <benjamin.r.shelton@protonmail.com>
2023-03-22 16:43:10 -0400

    PICO-8 saves symlink was backwards

--- functions.sh
+++ functions.sh
@@ -1312,7 +1312,7 @@ finit() {
   # PICO-8
   dir_prep "$bios_folder/pico-8" "$HOME/.lexaloffle/pico-8" # Store binary and config files together. The .lexaloffle directory is a hard-coded location for the PICO-8 config file, cannot be changed
   dir_prep "$roms_folder/pico8" "$bios_folder/pico-8/carts" # Symlink default game location to RD roms for cleanliness (this location is overridden anyway by the --root_path launch argument anyway)
-  dir_prep "$bios_folder/pico-8/cdata" "$saves_folder/pico-8" # PICO-8 saves folder
+  dir_prep "$saves_folder/pico-8" "$bios_folder/pico-8/cdata"  # PICO-8 saves folder
 
   (
   ra_init
