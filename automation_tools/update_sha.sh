b3e750cb86431ff4257ac2c41f76c75e9f6fcc5f
icenine451 <benjamin.r.shelton@protonmail.com>
2023-04-13 15:02:55 -0400

    While loop troubleshooting

--- automation_tools/update_sha.sh
+++ automation_tools/update_sha.sh
@@ -5,6 +5,7 @@
 
 rd_manifest=${GITHUB_WORKSPACE}/net.retrodeck.retrodeck.yml
 sha_update_list=${GITHUB_WORKSPACE}/automation_tools/sha_update_list.cfg
+counter=5
 
 echo "Manifest location: $rd_manifest"
 echo "Hash update list location: $sha_update_list"
@@ -12,6 +13,12 @@ echo
 echo "Update list contents:"
 cat "$sha_update_list"
 echo
+
+while [ $counter -gt 0 ]
+do
+  echo $counter
+  counter=$(( $counter - 1 ))
+done
 
 while IFS="^" read -r url placeholder
 do
