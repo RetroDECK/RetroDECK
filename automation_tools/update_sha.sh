db8b118626eda6564f45b60415d78aa8db10ee2c
icenine451 <benjamin.r.shelton@protonmail.com>
2023-04-13 11:51:43 -0400

    SHA placeholder automation debugging

--- automation_tools/update_sha.sh
+++ automation_tools/update_sha.sh
@@ -6,8 +6,14 @@
 rd_manifest=${GITHUB_WORKSPACE}/net.retrodeck.retrodeck.yml
 sha_update_list=${GITHUB_WORKSPACE}/automation_tools/sha_update_list.cfg
 
+echo "Manifest location: $rd_manifest"
+echo "Hash update list location: $sha_update_list"
+echo
+
 while IFS="^" read -r url placeholder
 do
+  echo "Placeholder text: $placeholder"
+  echo "URL to hash: $url"
   hash=$(curl -sL "$url" | sha256sum | cut -d ' ' -f1)
   sed -i 's^'"$placeholder"'^'"$hash"'^' $rd_manifest
 done < $sha_update_list
