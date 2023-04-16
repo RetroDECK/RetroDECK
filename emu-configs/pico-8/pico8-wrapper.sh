ec123bb346988774560bb268251a19f35738b5ef
icenine451 <59938822+icenine451@users.noreply.github.com>
2023-04-16 12:35:50 -0400

    PICO-8 binary wrapper

---
+++ emu-configs/pico-8/pico8-wrapper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is a wrapper function for PICO8, as ES-DE does not appear to be able to find it outside of the /app/bin location.
+# Users should still put the real binary in the ~/retrodeck/bios/pico-8/ location
+
+~/retrodeck/bios/pico-8/pico8
