576b9f6855cbdfe76271f4a6881d86a205efe76f
XargonWan <XargonWan@gmail.com>
2023-08-31 10:32:51 +0200

    comments [skip ci]

--- automation_tools/install_dependencies.sh
+++ automation_tools/install_dependencies.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# This scritp is installing the required dependencies to correctly run the pipeline and buold the flatpak
+
 sudo apt install -y flatpak flatpak-builder p7zip-full xmlstarlet bzip2 curl
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
