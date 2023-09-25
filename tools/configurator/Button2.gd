1d9ba2a124eb927937c98fe20ce0f32b0e440922
WallK <wallykrasiy@gmail.com>
2023-09-25 11:10:38 +0300

    Add progress bar, CheckButton and comments

--- tools/configurator/Button2.gd
+++ tools/configurator/Button2.gd
@@ -4,9 +4,7 @@ func _ready():
 	self.pressed.connect(self._button_pressed)
 
 func _button_pressed():
-	var output = []
-#	var exit_code = OS.execute("ls", ["-l", "/"], output, true)
-	var exit_code = OS.execute("bash", ["touch_external.sh"], output)
+	var output := [] #[] is shared, Array isn't (maybe fixed in 4.1)
+	var exit_code := OS.execute("bash", ["touch_external.sh"], output)
 	print("exit code: ", exit_code)
-	print("output: ",
-	 output)
+	print("output: ", output)
