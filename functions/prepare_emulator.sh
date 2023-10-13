bba5d05531f21983288088ebee5c6b523ad13fc7
XargonWan <xargonwan@gmail.com>
2023-10-13 10:15:40 +0200

    Vita3K: fixed config.yml seds path

--- functions/prepare_emulator.sh
+++ functions/prepare_emulator.sh
@@ -685,7 +685,7 @@ prepare_emulator() {
     # copying config file
     cp -fvr "$emuconfigs/vita3k/config.yml" "/var/data/Vita3K"
     # TODO: this step is to be done properly: Replacing RETRODECKHOMEDIR placeholder
-    sed -i 's#RETRODECKHOMEDIR#'$rdhome'#g' "/var/config/Vita3K/config.yml"
+    sed -i 's#RETRODECKHOMEDIR#'$rdhome'#g' "/var/data/Vita3K/config.yml"
 
     # copying vita user config
     cp -fvr "$emuconfigs/vita3k/ux0" "$bios_folder/Vita3K/Vita3K"
