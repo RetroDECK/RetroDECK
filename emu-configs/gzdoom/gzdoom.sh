9045e5d0502a0e5a5891e0275c74ffb1173a3853
XargonWan <xargonwan@gmail.com>
2023-11-24 14:12:34 +0100

    GZDOOM: added doom_complete.pk3 to the wrapper as an iwad

--- emu-configs/gzdoom/gzdoom.sh
+++ emu-configs/gzdoom/gzdoom.sh
@@ -6,7 +6,9 @@ LOG_FILE="$rdhome/.logs/gzdoom.log"
 IWAD_FILES=("DOOM1.WAD" "DOOM.WAD" "DOOM2.WAD" "DOOM2F.WAD" "DOOM64.WAD" "TNT.WAD"
             "PLUTONIA.WAD" "HERETIC1.WAD" "HERETIC.WAD" "HEXEN.WAD" "HEXDD.WAD"
             "STRIFE0.WAD" "STRIFE1.WAD" "VOICES.WAD" "CHEX.WAD"
-            "CHEX3.WAD" "HACX.WAD" "freedoom1.wad" "freedoom2.wad" "freedm.wad") # unlicenced iwads
+            "CHEX3.WAD" "HACX.WAD" "freedoom1.wad" "freedoom2.wad" "freedm.wad" # unlicenced iwads
+            "doom_complete.pk3"                                                 # this includes them all
+            )
 
 # Convert file name to uppercase for case-insensitive comparison
 provided_file=$(echo "$1" | tr '[:lower:]' '[:upper:]')
