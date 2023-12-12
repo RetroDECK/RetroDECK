cee6450dfd2c887f27521f6bc250148d3e42d985
XargonWan <xargonwan@gmail.com>
2023-12-12 12:35:57 +0100

    SUBMODULES: updated

--- automation_tools/force_update_submodules.sh
+++ automation_tools/force_update_submodules.sh
@@ -9,12 +9,12 @@ rm -rf .git/modules/rd-submodules/retroarch
 rm rd-submodules/ryujinx
 git rm -rf --cached rd-submodules/ryujinx
 rm -rf .git/modules/rd-submodules/ryujinx
-rm -rf shared-modules
-git rm -rf --cached shared-modules
-rm -rf .git/modules/shared-modules
+rm -rf rd-submodules/shared-modules
+git rm -rf --cached rd-submodules/shared-modules
+rm -rf .git/modules/rd-submodules/shared-modules
 
 git submodule init
-git submodule add https://github.com/flathub/shared-modules.git
+git submodule add https://github.com/flathub/shared-modules.git rd-submodules/shared-modules
 git submodule add https://github.com/flathub/org.libretro.RetroArch rd-submodules/retroarch
 git submodule add https://github.com/flathub/org.ryujinx.Ryujinx rd-submodules/ryujinx
 
