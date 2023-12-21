1db197219506f0a98278e602273c1a8c4f8b3e7b
XargonWan <xargonwan@gmail.com>
2023-12-21 13:43:35 +0100

    WORKFLOW: refact cooker-persistent

--- automation_tools/flatpak_build_bundle.sh
+++ automation_tools/flatpak_build_bundle.sh
@@ -2,17 +2,8 @@
 
 # This is building the bundle RetroDECK.flatpak after the download and build steps are done
 
-if [ $PERSISTENCE==true ]; then
-    GITHUB_WORKSPACE_BACKUP="$GITHUB_WORKSPACE"
-    GITHUB_WORKSPACE="$HOME/cooker-persistent"
-fi
-
 if [ "${GITHUB_REF##*/}" = "main" ]; then
-    flatpak build-bundle ${GITHUB_WORKSPACE}/.local $GITHUB_WORKSPACE/RetroDECK.flatpak net.retrodeck.retrodeck
+    flatpak build-bundle "${GITHUB_WORKSPACE}/.local" "$GITHUB_WORKSPACE/RetroDECK.flatpak" net.retrodeck.retrodeck
 else
-    flatpak build-bundle ${GITHUB_WORKSPACE}/.local $GITHUB_WORKSPACE/RetroDECK-cooker.flatpak net.retrodeck.retrodeck
-fi
-
-if [ $PERSISTENCE==true ]; then
-    GITHUB_WORKSPACE="$GITHUB_WORKSPACE_BACKUP"
+    flatpak build-bundle "${GITHUB_WORKSPACE}/.local" "$GITHUB_WORKSPACE/RetroDECK-cooker.flatpak" net.retrodeck.retrodeck
 fi
