95c6f73adc4bda5effafbdd79e485c884cc459b5
WallK <wallykrasiy@gmail.com>
2023-12-22 09:14:01 +0200

    Changed speech bubble to use tooltips instead Also, temporary enabled tooltip hover

--- tools/configurator/helper_text.gd
+++ tools/configurator/helper_text.gd
@@ -7,7 +7,7 @@ func _ready():
 	get_viewport().connect("gui_focus_changed", _on_focus_changed)
 
 func _on_focus_changed(selected_element:Control) -> void:
-	if selected_element != null and selected_element.has_meta("description"):
-		helper_text_node.text = selected_element.get_meta("description")
+	if selected_element != null and selected_element.get_tooltip():
+		helper_text_node.text = selected_element.tooltip_text
 	else:
 		helper_text_node.text = "Hey, there's no description"
