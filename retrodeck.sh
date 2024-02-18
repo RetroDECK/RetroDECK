a7bbfd3ae56c2ef0ee1e727289d6bf675bfaa7c6
Lx32 <loxenfabula@gmail.com>
2024-02-18 10:47:37 +0100

    Update retrodeck.sh with steam syncronization

--- retrodeck.sh
+++ retrodeck.sh
@@ -191,4 +191,12 @@ fi
 
 # Normal Startup
 
+if [[ $steam_sync == "true" ]]; then
+  python3 /app/libexec/steam-sync/steam-sync.py &
+fi
+
 start_retrodeck
+
+if [[ $steam_sync == "true" ]]; then
+  touch /tmp/retrodeck_steam_sync_exit
+fi
