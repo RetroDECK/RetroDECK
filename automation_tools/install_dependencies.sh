f18eff92436671c6fed3800ece4169deadb05212
XargonWan <XargonWan@gmail.com>
2024-02-23 10:07:27 +0100

    DEPENDENCIES: added jq as workflows dependency [skip ci]

--- automation_tools/install_dependencies.sh
+++ automation_tools/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This scritp is installing the required dependencies to correctly run the pipeline and buold the flatpak
 
-sudo apt install -y flatpak flatpak-builder p7zip-full xmlstarlet bzip2 curl
+sudo apt install -y flatpak flatpak-builder p7zip-full xmlstarlet bzip2 curl jq
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --user --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
