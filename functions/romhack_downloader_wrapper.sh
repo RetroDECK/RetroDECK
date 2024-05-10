f7da934900f2de607de05992cbf1b24a450331ab
Libretto <>
2024-05-10 11:04:38 +0200

    trigger new build

--- functions/romhack_downloader_wrapper.sh
+++ functions/romhack_downloader_wrapper.sh
@@ -4,6 +4,6 @@ romhack_downloader_wrapper() {
   # turn ~ into path
   eval expanded_roms_folder="$roms_folder"
   
-  cd /app/libexec/romhack_downloader
+  cd /app/libexec/romhack_downloader 
   python3 main.py --roms-folder "$expanded_roms_folder" "$@"
 }
