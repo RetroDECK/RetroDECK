36f5994d1b008f745c5b0f8aaa2343cfe9fb5e10
monkeyx-net <tim@monkeyx.net>
2024-06-29 11:02:56 +0100

    On branch feat/ruffle  Your branch is ahead of 'origin/feat/ruffle' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   emu-configs/ruffle/ruffle-wrapper.sh
    	deleted:    emu-configs/ruffle/ruffle.log
    	modified:   net.retrodeck.retrodeck.yml

---
+++ emu-configs/ruffle/ruffle-wrapper.sh
@@ -0,0 +1,8 @@
+source /app/libexec/global.sh
+
+#Check if Steam Deck in Desktop Mode
+if [[ $(check_desktop_mode) == "true" ]]; then
+    ruffle --graphics vulkan --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
+else
+    ruffle --graphics gl --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
+fi
