b830113a166dd146e8591ffa34a8e9d2e13de08e
monkeyx-net <tim@monkeyx.net>
2024-06-29 19:41:59 +0100

    On branch feat/ruffle  Your branch is up to date with 'origin/feat/ruffle'.
     Changes to be committed:
    	modified:   emu-configs/ruffle/ruffle-wrapper.sh

--- emu-configs/ruffle/ruffle-wrapper.sh
+++ emu-configs/ruffle/ruffle-wrapper.sh
@@ -2,7 +2,7 @@ source /app/libexec/global.sh
 
 #Check if Steam Deck in Desktop Mode
 if [[ $(check_desktop_mode) == "true" ]]; then
-    ruffle --graphics vulkan --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
+    /app/share/ruffle --graphics vulkan --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
 else
-    ruffle --graphics gl --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
+    /app/share/ruffle --graphics gl --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
 fi
