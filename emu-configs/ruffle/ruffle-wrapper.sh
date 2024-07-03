da88e5784fa59695d65899a5b15be83ca0e2bb4a
monkeyx-net <tim@monkeyx.net>
2024-07-03 19:36:29 +0100

    Ruffle as an archive :)
     On branch feat/ruffle
     Your branch is up to date with 'origin/feat/ruffle'.
    
     Changes to be committed:
    	modified:   emu-configs/ruffle/ruffle-wrapper.sh
    	modified:   net.retrodeck.retrodeck.yml

--- emu-configs/ruffle/ruffle-wrapper.sh
+++ emu-configs/ruffle/ruffle-wrapper.sh
@@ -4,7 +4,7 @@ source /app/libexec/global.sh
 
 #Check if Steam Deck in Desktop Mode
 if [[ $(check_desktop_mode) == "true" ]]; then
-    /app/share/ruffle/ruffle --graphics vulkan --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
+    ruffle --graphics vulkan --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
 else
-    /app/share/ruffle/ruffle --graphics gl --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
+    ruffle --graphics gl --config /var/data/ruffle --save-directory $saves_folder/ruffle --fullscreen "$@"
 fi
