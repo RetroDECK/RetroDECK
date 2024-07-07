ef86142d36b10979b9f92d40a6c698b2c4cd3745
Marco Matthijs <marco.matthijs@hotmail.com>
2024-07-07 22:04:53 +0200

    CLOUD_SYNC: de-hard coding #1

--- functions/cloud_sync.sh
+++ functions/cloud_sync.sh
@@ -4,7 +4,7 @@
 # - remove hard code
 # - add multi-user support
 # - save remote name somewhere (and maybe make customisable remote name)
-# - optional: back up to mutliple remotes (by replacing ~/retrodeck with a different remote)
+# - optional: back up to mutliple remotes (by replacing $rdhome with a different remote)
 # - add exclusion options (to have multiple playthroughs for different devices)
 
 
@@ -46,22 +46,22 @@ set_cloud() {
     esac
     rclone mkdir RetroDECK:/RetroDECK
     rclone mkdir RetroDECK:/RetroDECK_backup
-    touch ~/retrodeck/RCLONE_TEST
+    touch $rdhome/RCLONE_TEST
     mkdir ~/retrodeck_backup
     #TODO: discuss which other directories are eligable for syncing, and add them to the include flag (or user choice). 
-    rclone --copy-links --check-first bisync --resync --resync-mode $2 ~/retrodeck RetroDECK:/RetroDECK --include "{saves,screenshots}/**" --backup-dir1 ~/retrodeck_backup --backup-dir2 RetroDECK:/RetroDECK_backup
+    rclone --copy-links --check-first bisync --resync --resync-mode $2 $rdhome RetroDECK:/RetroDECK --include "{saves,screenshots}/**" --backup-dir1 ~/retrodeck_backup --backup-dir2 RetroDECK:/RetroDECK_backup
 }
 
 sync_cloud() {
-    # --max-delete PERCENT Safety check on maximum percentage of deleted files allowed. If exceeded, the bisync run will abort. (default: 50%)?
-    rclone --copy-links --check-first bisync --recover --no-slow-hash --check-access --conflict-resolve $1 ~/retrodeck RetroDECK:/RetroDECK --include "{saves,screenshots}/**" --backup-dir1 ~/retrodeck_backup --backup-dir2 RetroDECK:/RetroDECK_backup
+    # --max-delete PERCENT: Safety check on maximum percentage of deleted files allowed. If exceeded, the bisync run will abort. (default: 50%)?
+    rclone --copy-links --check-first bisync --recover --no-slow-hash --check-access --conflict-resolve $1 $rdhome RetroDECK:/RetroDECK --include "{saves,screenshots}/**" --backup-dir1 ~/retrodeck_backup --backup-dir2 RetroDECK:/RetroDECK_backup
 }
 
-# --update Skip files that are newer on the destination?
+# --update: Skip files that are newer on the destination?
 push_cloud() {
-    rclone --copy-links --check-first sync --check-first ~/retrodeck RetroDECK:/RetroDECK --include "{saves,screenshots}/**" --backup-dir RetroDECK:/RetroDECK_backup
+    rclone --copy-links --check-first sync --check-first $rdhome RetroDECK:/RetroDECK --include "{saves,screenshots}/**" --backup-dir RetroDECK:/RetroDECK_backup
 }
 
 pull_cloud() {
-    rclone --copy-links --check-first sync --check-first RetroDECK:/RetroDECK ~/retrodeck --include "{saves,screenshots}/**" --backup-dir ~/retrodeck_backup
+    rclone --copy-links --check-first sync --check-first RetroDECK:/RetroDECK $rdhome --include "{saves,screenshots}/**" --backup-dir ~/retrodeck_backup
 }
