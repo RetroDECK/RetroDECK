e47c9503fe4487f76b9b512a1a1eb24a0ca17962
XargonWan <XargonWan@gmail.com>
2024-07-12 17:26:45 +0900

    DEVELOPER_TOOLBOX: disabled appdata management in local build

--- developer_toolbox/build_retrodeck_locally.sh
+++ developer_toolbox/build_retrodeck_locally.sh
@@ -17,7 +17,8 @@ automation_tools/install_dependencies.sh
 automation_tools/cooker_build_id.sh
 automation_tools/pre_build_automation.sh
 automation_tools/cooker_flatpak_portal_add.sh
-automation_tools/appdata_management.sh
+# THIS SCRIPT IS BROKEN HENCE DISABLED FTM
+# automation_tools/appdata_management.sh
 automation_tools/flatpak_build_download_only.sh
 automation_tools/flatpak_build_only.sh
 automation_tools/flatpak_build_bundle.sh
