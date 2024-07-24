0b9341e56fee8af26f6f92449dc97f1414c121ce
XargonWan <XargonWan@gmail.com>
2024-07-24 16:56:30 +0900

    Whoops typo

--- functions/global.sh
+++ functions/global.sh
@@ -3,7 +3,7 @@
 # This file is containing some global function needed for the script such as the config file tools
 
 # pathing the retrodeck components provided libraries
-export LD_LIBRAY_PATH="$LD_LIBRAY_PATH:/app/retrodeck/lib/"
+export LD_LIBRARY_PATH="/app/retrodeck/lib:$LD_LIBRARY_PATH"
 
 source /app/libexec/050_save_migration.sh
 source /app/libexec/checks.sh
