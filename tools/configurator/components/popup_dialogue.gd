29f251596b7e48c7d8b8bc32a74c73f7e800e60d
Rekku <rekku@retrodeck.net>
2024-09-01 17:44:16 +0100

    Tidied parameter code

--- tools/configurator/components/popup_dialogue.gd
+++ tools/configurator/components/popup_dialogue.gd
@@ -9,24 +9,29 @@ var parameters: Array
 
 func _ready():
 	$".".theme = custom_theme
-	#%title_label.text = "FRED"
+	var args = OS.get_cmdline_args()
+	for arg in range(args.size()):
+		if args[arg] == "--dialogue" and arg + 1 < args.size():
+			print ("TEST")
+		elif args[arg] == "--title" and arg + 1 < args.size():
+			%title_label.text = args[arg + 1]
+		elif args[arg] == "--content" and arg + 1 < args.size():
+			%content_rtl.text = args[arg + 1]
+		elif args[arg] == "--command" and arg + 1 < args.size():
+			command = args[arg + 1]
+		elif args[arg] == "--parameters" and arg + 1 < args.size():
+			parameters.append(args[arg + 1])
+		elif args[arg] == "--fullscreen" and arg + 1 < args.size():
+			DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_FULLSCREEN)
 
-func _process(delta):
+#func _process(delta):
+	#if Input.is_action_pressed("back_button"):
+		#get_tree().quit()
+
+func _input(event):
 	if Input.is_action_pressed("back_button"):
 		get_tree().quit()
-		
-func set_title(new_title):
-	%title_label.text = new_title
 	
-func set_content(new_content_text):
-	%content_rtl.text=new_content_text	
-	
-func set_ok_command(ok_command):
-	command = ok_command
-
-func set_parameters(new_parameters: Array):
-	parameters = new_parameters
-
 func _on_cancel_pressed():
 	class_functions.log_parameters[2] = class_functions.log_text + "Exited dialogue"
 	class_functions.execute_command(class_functions.wrapper_command,class_functions.log_parameters, false)
@@ -39,6 +44,3 @@ func _on_ok_button_pressed() -> void:
 	class_functions.log_parameters[2] = class_functions.log_text + "Exit code: " + str(result["exit_code"])
 	%content_rtl.text = result["output"]
 	class_functions.execute_command(class_functions.wrapper_command,class_functions.log_parameters, false)
-
-	
-	
