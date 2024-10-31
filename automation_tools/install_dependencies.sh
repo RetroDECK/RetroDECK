a492fdb0a659a62be7f1d80524413e2767a97610
XargonWan <XargonWan@gmail.com>
2024-10-31 10:07:45 +0900

    Revert "DEPENDENCIES: added appstream-compose"
    This reverts commit 79632236271ec8c46eb963c0738d3ea92e7681a2.

--- automation_tools/install_dependencies.sh
+++ automation_tools/install_dependencies.sh
@@ -10,17 +10,17 @@ done
 
 case "$pkg_mgr" in
   apt)
-    sudo apt install -y flatpak flatpak-builder p7zip-full xmlstarlet bzip2 curl jq appstream-compose
+    sudo apt install -y flatpak flatpak-builder p7zip-full xmlstarlet bzip2 curl jq
     ;;
   pacman)
-    sudo pacman -S --noconfirm flatpak flatpak-builder p7zip xmlstarlet bzip2 appstream-compose
+    sudo pacman -S --noconfirm flatpak flatpak-builder p7zip xmlstarlet bzip2
     ;;
   rpm-ostree)
     echo "When using a distro with rpm-ostree, you shouldn't build directly on the host. Try using a distrobox."
     exit 1
     ;;
   dnf)
-    sudo dnf install -y flatpak flatpak-builder p7zip p7zip-plugins xmlstarlet bzip2 curl appstream-compose
+    sudo dnf install -y flatpak flatpak-builder p7zip p7zip-plugins xmlstarlet bzip2 curl
     ;;
   *)
     echo "Package manager $pkg_mgr not supported. Please open an issue."
