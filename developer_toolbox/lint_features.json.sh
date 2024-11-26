142aab120eee5e36a71a0922b838c65a53ebd8e2
monkeyx-net <tim@monkeyx.net>
2024-11-26 14:44:40 +0000

    issue with lint_features.json.sh?

--- developer_toolbox/lint_features.json.sh
+++ developer_toolbox/lint_features.json.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-features=config/retrodeck/reference_lists/features.json
+config="/app/retrodeck/config"  
+features=$config/retrodeck/reference_lists/features.json
 
 jq . $features > $features.tmp && mv -f $features.tmp $features
