f273bc92cc7a2e2fd6f7c28acd6b95d0c9254462
XargonWan <XargonWan@gmail.com>
2024-12-10 10:27:09 +0900

    WORKFLOW: added a first implementation of library checker in the automation tests

---
+++ automation_tools/search_missing_libs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Search for missing libraries
+missing_libs=$(flatpak run --command=sh net.retrodeck.retrodeck -c \
+"find \${FLATPAK_DEST}/bin -type f -exec ldd {} + 2>/dev/null | grep 'not found' | awk '{print \$1}' | sort | uniq")
+
+# If there is any missing library it will be printed and the step will fail
+if [ -n "$missing_libs" ]; then
+    echo "The following libraries are missing:"
+    echo "$missing_libs"
+    exit 1
+else
+    echo "TEST OK: No missing libraries are found"
+fi     
