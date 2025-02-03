1f38150599478541c59aea9ae01aca2a2fa7ed93
XargonWan <XargonWan@gmail.com>
2025-02-03 10:46:59 +0900

    WORKFLOW: added proper push step if in gtihub runner - fix2 [skip ci]

--- automation_tools/flathub_push_main.sh
+++ automation_tools/flathub_push_main.sh
@@ -82,6 +82,7 @@ fi
 if [ -n "${GITHUB_WORKFLOW}" ]; then
     echo "RD_BRANCH=$rd_branch" >> $GITHUB_ENV
     echo "RELNAME=$relname" >> $GITHUB_ENV
+    echo "FOLDER_TO_PUSH="$gits_folder/flathub"" >> $GITHUB_ENV
 else
     git add .
     git commit -m "Updated flathub/net.retrodeck.retrodeck to v$relname from RetroDECK/$rd_branch"
