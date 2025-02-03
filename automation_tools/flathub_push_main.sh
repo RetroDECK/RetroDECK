6acb4a68054e2f642c763f19dc86be36b7148b7c
XargonWan <XargonWan@gmail.com>
2025-02-03 14:11:36 +0900

    WORKFLOW: trying using gh instead - fix9 [skip ci]

--- automation_tools/flathub_push_main.sh
+++ automation_tools/flathub_push_main.sh
@@ -77,10 +77,10 @@ EOF
 
 # If we are in a GitHub workflow...
 if [ -n "${GITHUB_WORKFLOW}" ]; then
-    git config --local user.name "$GIT_NAME"
-    git config --local user.email "$GIT_MAIL"
-    git config --local credential.helper store
-    echo "${GITHUB_TOKEN}" | gh auth login --with-token
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_MAIL"
+    git config --global credential.helper store
+    gh auth login
 elif [[ -z $(git config --get user.name) || -z $(git config --get user.email) ]]; then
     read -p "No git user.name set, please enter your name: " git_username
     git config --local user.name "$git_username"
