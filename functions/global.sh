54c04fa5990a69af0a634896f14757181b04461a
XargonWan <XargonWan@gmail.com>
2025-02-13 11:08:04 +0900

    CHORE: comments

--- functions/global.sh
+++ functions/global.sh
@@ -238,6 +238,6 @@ logs_folder="$rdhome/logs"                # The path of the logs folder, here we
 steamsync_folder="$rdhome/.sync"          # Folder containing all the steam sync launchers for SRM
 steamsync_folder_tmp="$rdhome/.sync-tmp"  # Temp folder containing all the steam sync launchers for SRM
 cheats_folder="$rdhome/cheats"            # Folder containing all the cheats for the emulators
-backups_folder="$rdhome/backups"            # Folder containing all the RetroDECK backups
+backups_folder="$rdhome/backups"          # Folder containing all the RetroDECK backups
 
 export GLOBAL_SOURCED=true
