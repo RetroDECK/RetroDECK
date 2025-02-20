a37ae0ceeb85af41d09a3f2e7d96f79c731de0e0
XargonWan <XargonWan@gmail.com>
2025-02-20 09:31:00 +0900

    ZENITY: removed gdk scaling

--- functions/other_functions.sh
+++ functions/other_functions.sh
@@ -301,9 +301,9 @@ rd_zenity() {
   # This function replaces the standard 'zenity' command and filters out annoying GTK errors on Steam Deck
   export GUI="zenity"
 
-  env GDK_SCALE=1.5 \
-      GDK_DPI_SCALE=1.5 \
-      zenity 2> >(grep -v 'Gtk' >&2) "$@"
+  # env GDK_SCALE=1.5 \
+  #     GDK_DPI_SCALE=1.5 \
+  zenity 2> >(grep -v 'Gtk' >&2) "$@"
 
   local status=${PIPESTATUS[0]}  # Capture the exit code of 'zenity'
 
