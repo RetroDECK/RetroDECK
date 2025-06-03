e51116f76b08121a661d74d52286d36b904cc1cd
XargonWan <XargonWan@gmail.com>
2025-06-03 10:35:20 +0900

    feat(inject): add script for injecting framework and components into RetroDECK installation

--- developer_toolbox/inject.sh
+++ developer_toolbox/inject.sh
@@ -2,7 +2,7 @@
 
 # WARNING: run this script from the project root folder, not from here!!
 
-# This script is used to inject framework and config files inside a RetroDECK cooker installation
+# This script is used to inject framework, config files and components inside a RetroDECK Cooker installation
 # To apply the injected config you have to reset the targeted component from the Configurator
 # Please know what you're doing, if you need to undo this you need to completely uninstall and reinstall RetroDECK flatpak
 # Please note that this may create a dirty situation where older files are still in place as the action is add and overwrite
@@ -47,16 +47,19 @@ sudo cp -vfr "config/"** "$app/retrodeck/config/"
 sudo cp -vfr "tools" "$app"
 sudo cp -vfr "retrodeck.sh" "$app/bin/"
 sudo cp -vfr "functions/"** "$app/libexec/"
-sudo cp -vfr "config/gzdoom/gzdoom.sh" "$app/bin/"
-sudo cp -vfr "config/ruffle/ruffle-rdwrapper.sh" "$app/bin/"
 sudo cp -vfr "net.retrodeck.retrodeck.metainfo.xml" "$app/share/metainfo/net.retrodeck.retrodeck.metainfo.xml"
+echo ""
 
-read -p "Do you also want to inject RetroDECK Components? (Y/n): " inject_components
+echo "Components injection is possible, be sure that your components folder contains the desired components."
+echo "If you want to update the components, run the 'manage_components.sh' script first and re-run this script."
+echo ""
+read -p "Do you want to inject RetroDECK Components? (Y/n): " inject_components
+
 inject_components=${inject_components:-Y}
 if [[ "$inject_components" =~ ^[Yy]$ ]]; then
     if [ -d "components" ]; then
-        sudo cp -vfr "components" "$app/"
-        echo "Components injected."
+        FLATPAK_DEST="$app"
+        source "automation_tools/install_components.sh"
     else
         echo "No 'components' directory found to inject."
     fi
