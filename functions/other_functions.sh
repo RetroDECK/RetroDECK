9f1b7ad9a5262decc0178c04a27f38f978c31bdf
icenine451 <benjamin.r.shelton@protonmail.com>
2025-11-19 10:55:12 -0500

    Remove duplicate functions which have been moved to the framework component_functions.sh file [skip ci]

--- functions/other_functions.sh
+++ functions/other_functions.sh
@@ -1348,20 +1348,6 @@ convert_to_markdown() {
       -e '/<[^>]*>/d' > "$output_file" # Remove any other XML tags and output to .md file
 }
 
-portmaster_show(){
-  log d "Setting PortMaster visibility in ES-DE"
-  if [ "$1" = "true" ]; then
-      log d "\"$roms_path/portmaster/PortMaster.sh\" is not found, installing it"
-      install -Dm755 "$XDG_DATA_HOME/PortMaster/PortMaster.sh" "$roms_path/portmaster/PortMaster.sh" && log d "PortMaster is correctly showing in ES-DE"
-      set_setting_value "$rd_conf" "portmaster_show" "true" retrodeck "options"
-  elif [ "$1" = "false" ]; then
-    rm -rf "$roms_path/portmaster/PortMaster.sh" && log d "PortMaster is correctly hidden in ES-DE"
-    set_setting_value "$rd_conf" "portmaster_show" "false" retrodeck "options"
-  else
-    log e "\"$1\" is not a valid choice, quitting"
-  fi
-}
-
 open_component(){
   local command="$1"
   shift
