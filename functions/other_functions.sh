011e292a3a05bb9d42a274a17e2da5f3951ddc68
laz <thelazorne@protonmail.com>
2025-12-03 18:44:32 +0100

    srm text finit [skip ci]

--- functions/other_functions.sh
+++ functions/other_functions.sh
@@ -1837,8 +1837,8 @@ install_retrodeck_controller_profile_and_add_to_steam() {
   install_retrodeck_controller_profile
   add_retrodeck_to_steam
   
-  rd_zenity --question --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" --title "RetroDECK Initial Install - 🚂 Enable Steam Sync 🚂" --cancel-label="No 🟥 " --ok-label "Yes 🟢" \
-    --text="Would you like to enable automatic Steam Sync of your Favorites in ES-DE?"
+  rd_zenity --question --no-wrap --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" --title "RetroDECK Initial Install - 🚂 Steam Syncronization 🚂" --cancel-label="No 🟥 " --ok-label "Yes 🟢" \
+    --text="Enable Steam synchronization?\n\nThis will scan your games for any 🌟 <span foreground=\"$purple\"><b>Favorited</b></span> 🌟 games in ES-DE and add them to your Steam library as individual entries.\n\nYou will need to restart Steam for the changes to take effect."
 
   if [[ $? == 0 ]]
     configurator_enable_steam_sync
