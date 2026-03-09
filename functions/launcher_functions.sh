7d3422e3be48f8c1ff3394ee967eaf57836f5d55
icenine451 <benjamin.r.shelton@protonmail.com>
2026-03-09 11:30:12 -0400

    Add launcher_functions.sh library

---
+++ functions/launcher_functions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This library provides minimal function loading for scripts working outside of the application shell (such as ones called by /bin/bash <script> or exec) without the need to source global.sh
+# All application variables are still available to all scripts as they are exported to the environment
+
+source /app/libexec/cleanup.sh
+source /app/libexec/logger.sh
