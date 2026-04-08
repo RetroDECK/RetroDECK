d9caf7093ae9b65ff3a06f6c1be1df81ca596d53
icenine451 <benjamin.r.shelton@protonmail.com>
2026-04-08 16:14:58 -0400

    Add initial configurator_move_folder_choice_dialog function

--- functions/configurator_functions.sh
+++ functions/configurator_functions.sh
@@ -1344,3 +1344,16 @@ configurator_toggle_retroengine_event_scripts_dialog() {
     fi
   fi
 }
+
+configurator_move_folder_choice_dialog() {
+  build_zenity_component_paths_menu_array "paths_to_choose"
+
+  choice=$(rd_zenity --list --title="RetroDECK Configurator - Move Folder" --cancel-label="Back" \
+    --window-icon="/app/share/icons/hicolor/scalable/apps/net.retrodeck.retrodeck.svg" --width=1200 --height=720 \
+    --column="Folder" --column="Description" --column="Current Location" --column="path_key" --print-column=4 --hide-column=4 \
+    "${paths_to_choose[@]}")
+
+  if [[ -n "$choice" ]]l then
+    configurator_move_folder_dialog "$choice"
+  fi
+}
