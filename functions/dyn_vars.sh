4d4ece4aa567bcea1be481ca5fce35d3be1a9e4c
icenine451 <benjamin.r.shelton@protonmail.com>
2026-08-21 10:44:11 -0400

    Move component_manifest_cache_file to XDG_CACHE_HOME location

--- functions/dyn_vars.sh
+++ functions/dyn_vars.sh
@@ -5,4 +5,4 @@
 export rd_conf="$XDG_CONFIG_HOME/retrodeck/retrodeck.json"                                                      # RetroDECK config file path
 export rd_lockfile="$XDG_CONFIG_HOME/retrodeck/.lock"                                                           # Where the lockfile is located
 export folder_iconsets_dir="$XDG_CONFIG_HOME/retrodeck/graphics/folder-iconsets"
-export component_manifest_cache_file="$XDG_CONFIG_HOME/retrodeck/retrodeck_manifest_cache.json"
+export component_manifest_cache_file="$XDG_CACHE_HOME/retrodeck/retrodeck_manifest_cache.json"
